7bcce201f962f73c0606a7e92da5fe6087b13bec
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-05-21 15:10:24 -0500

    Services Layer developed

--- ShoppingAPI_Jueves/ShoppingAPI_Jueves/Domain/interfaces/ICountryService.cs
+++ ShoppingAPI_Jueves/ShoppingAPI_Jueves/Domain/interfaces/ICountryService.cs
@@ -5,9 +5,9 @@ namespace ShoppingAPI_Jueves.Domain.interfaces
     public interface ICountryService
     {
         Task <IEnumerable<Country>> GetCountriesAsync(); // es una firma de metodos
+        Task<Country> GetCountryByIdAsync(Guid id);
         Task<Country> CreateCountryAsync(Country country);
-        Task<Country> GetCountryById(Guid id);
         Task<Country> EditCountryAsync(Country country);
-        Task<Country> DeletCountryById(Guid id);
+        Task<Country> DeletCountryAsync(Guid id);
     }
 }
